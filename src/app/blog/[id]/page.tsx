935ab2545ba482503c05292be15697264907b3bd
ABHIJIT8008 <abhijitgupta196@gmail.com>
2025-09-24 12:19:28 +0530

    fix: definitively correct blog page types

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -4,9 +4,15 @@ import { getBlog } from "@/app/blog/blogs/blogs";
 import { notFound } from "next/navigation";
 import React from "react";
 
-// The props are correctly typed here. The component can be async.
-const page = async ({ params }: { params: { id: string } }) => {
-  // The 'await' keyword is removed here. This is the fix.
+// Define the correct props type inline
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// Use the correct 'Props' type
+const BlogPostPage = ({ params }: Props) => {
   const { id } = params;
   const blog = getBlog(id);
 
@@ -14,19 +20,15 @@ const page = async ({ params }: { params: { id: string } }) => {
     notFound();
   }
 
-  // const body = await markdownToHtml(blog.body);
-
   return (
-    <>
-      <article className="w-full p-8 mt-nav flex flex-col justify-center items-center dark:bg-trueGray-900">
-        <Image src={blog.img} alt={blog.alt} className="object-cover" />
-        <section className="w-full max-w-5xl">
-          <Title>{blog.title}</Title>
-          {blog.body}
-        </section>
-      </article>
-    </>
+    <article className="w-full p-8 mt-nav flex flex-col justify-center items-center dark:bg-trueGray-900">
+      <Image src={blog.img} alt={blog.alt} className="object-cover" />
+      <section className="w-full max-w-5xl">
+        <Title>{blog.title}</Title>
+        {blog.body}
+      </section>
+    </article>
   );
 };
 
-export default page;
+export default BlogPostPage;
